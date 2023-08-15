88557bf636eedd95ad5e52be5b53646c57508ffa
Ritesh Kumar <rit23kumar@gmail.com>
2023-08-15 20:10:27 +0530

    first commit

---
+++ src/examples/Sidenav/SidenavFooter.js
@@ -0,0 +1,42 @@
+/**
+=========================================================
+* MicroEdge Suite - v1.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-material-ui
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Link from "@mui/material/Link";
+
+// MicroEdge Suite components
+import ArgonButton from "components/ArgonButton";
+import ArgonBox from "components/ArgonBox";
+import ArgonTypography from "components/ArgonTypography";
+
+// MicroEdge Suite context
+import { useArgonController } from "context";
+
+// Images
+import icon from "assets/images/illustrations/icon-documentation.svg";
+
+function SidenavFooter() {
+  const [controller] = useArgonController();
+  const { miniSidenav, darkSidenav } = controller;
+
+  return (
+    <ArgonBox opacity={miniSidenav ? 0 : 1} sx={{ transition: "opacity 200ms linear" }}>
+      
+      
+    </ArgonBox>
+  );
+}
+
+export default SidenavFooter;
